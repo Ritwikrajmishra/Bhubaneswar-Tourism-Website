63f04a48380b92a76c79d439fc01eb37ee5d3176
Ritwikrajmishra <76164118+Ritwikrajmishra@users.noreply.github.com>
2022-05-05 23:42:28 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,59 @@
+function lingaraj_temple()
+
+{
+    window.location = "lingaraj.html";
+}
+
+function ram_mandir()
+
+{
+    window.location = "ram_mandir.html";
+}
+
+function dhauligiri()
+
+{
+    window.location = "dhauligiri.html";
+}
+
+function khandagiri()
+
+{
+    window.location = "khandagiri.html";
+}
+
+function nandankanan()
+
+{
+    window.location = "nandan_kanan.html";
+}
+
+function odisha_museum()
+
+{
+    window.location = "state_museum.html";
+}
+
+function tribal_museum()
+
+{
+    window.location = "tribal_museum.html";
+}
+
+function planetarium()
+
+{
+    window.location = "planetarium.html";
+}
+
+function sci_park()
+
+{
+    window.location = "sci_park.html";
+}
+
+function haat()
+
+{
+    window.location = "ekamra.html";
+}
